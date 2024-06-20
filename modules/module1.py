ceb897b2881b664e1d7dd2d00ecf52cc038ba99a
ArtLightOst <85809555+ArtLightOst@users.noreply.github.com>
2024-06-20 16:45:00 +0300

    Update module1.py

--- modules/module1.py
+++ modules/module1.py
@@ -1,39 +1,93 @@
 from service import ParentService, create_button, create_input, create_table, create_list, create_link
-import subprocess
+import subprocess, re
 
 class Service(ParentService):
 
+
     def public_print(self, data: dict) -> list[dict]:
-        result = subprocess.run("ls", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
-        text = result.stdout.split()
+
+        result = subprocess.run("cat all_lics.txt", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+
+        all_lics = []
+
+        for line in result.stdout.split('\n'):
+            if line:
+                lic = re.search("\"\d+.lic\"", line)
+                if lic[0]:
+                    all_lics.append(lic[0].replace('"', ''))
+
+        all_lics = dict().fromkeys(all_lics)
+
+        result = subprocess.run("cat used_lics.txt", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+        used_lics = {}
+
+        for line in result.stdout.split('\n'):
+            if line:
+                current = line.split()
+                pid = current[1]
+                current_lic = current[len(current) - 1]
+                current_lic = current_lic.split('/')
+                current_lic = current_lic[len(current_lic) - 1]
+                used_lics[pid] = current_lic.replace(")", "")
+
+        result = subprocess.run("cat host_lics.txt", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+
+        host_lics = {}
+
+        for line in result.stdout.split('\n'):
+            if line:
+                current = line.split()
+                pid = current[1]
+                index = current.index("-reghost")
+                host_lics[pid] = current[index + 1]
+
+        result = {}
+
+        for key in host_lics:
+            lic = used_lics.get(key)
+            if pid:
+                result[host_lics[key]] = lic
         
+        keys = set(all_lics.keys())
+        values = set(result.values())
+
+        diff = keys.difference(values)
+
+        free = list(diff)
+
+        formatted = []
+
+        for k in result:
+            formatted.append(
+                {
+                    "server": k,
+                    "lic": result[k] if result[k] else "Не используется"
+                }
+            )
+
         response = [
 
-            create_list(
-                id="list",
-                array=text
+            create_table(
+                id="lics",
+                headers="Сервер, Лиценизия",
+                data=formatted
             ),
-            create_button(
-		id = "button",
-		caption = "Кнопка",
-		module = "module1",
-		method = "print_some"
-	    )
-
+            *create_list(
+                id="free",
+                array=free,
+                caption="Свободные"
+            )    
+	    
         ]
         return response
 
-    def print_some(self, data: dict) -> list[dict]:
-        result = subprocess.run("systemctl start nginx", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
-        
-        return {"feedback": str(result)}
 
 instance = Service()
 config = {
 	"module1": {
-		"synonyme": "Модуль 1",
+		"synonyme": "Лицензирование",
 		"methods": {
-			"public_print": "Список файлов"
+			"public_print": "Использованные лицензии"
 			}
 		}
 
